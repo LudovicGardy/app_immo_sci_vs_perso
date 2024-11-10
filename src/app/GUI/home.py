f5a6e662c9f5bb2a57bfb6447ae3c90d0e046f11
Ludovic Gardy <ludovic.gardy@gmail.com>
2024-11-10 21:17:01 +0100

    removed useless comments

--- src/app/GUI/home.py
+++ src/app/GUI/home.py
@@ -2,6 +2,7 @@ import streamlit as st
 from src.app.calculations.investment_options import PersonalPurchase, SCIInvestment
 from src.app.utils.data_visualization import plot_total_costs, plot_annual_costs
 
+
 class Home:
     """Classe pour gérer les calculs et l'affichage des résultats."""
 
@@ -16,17 +17,17 @@ class Home:
 
         st.title("Comparaison des options d'investissement immobilier")
 
-        # Calculs
         option1 = PersonalPurchase(self.property_price, self.dividend_tax_rate)
         option1_result = option1.calculate()
 
-        option2 = SCIInvestment(self.rent_per_month, self.dividend_tax_rate, int(self.years))
+        option2 = SCIInvestment(
+            self.rent_per_month, self.dividend_tax_rate, int(self.years)
+        )
         option2_result = option2.calculate()
 
         tab1, tab2 = st.tabs(["📐 Calculs", "📊 Plots"])
 
         with tab1:
-            # Affichage des résultats
             st.write("## 1️⃣ Achat personnel avec dividendes d'une SASU")
             st.write("### 📃 Société d'exploitation: SASU")
             with st.container(border=True):
@@ -48,26 +49,24 @@ class Home:
             with st.container(border=True):
                 st.write(
                     f"📍 Loyer annuel payé: **{option2_result['annual_rent']:.2f} €**"
-                    )
+                )
                 st.write(
                     f"📍 Dividendes nets reçus de la SCI: **{option2_result['net_dividends_received']:.2f} €**"
                 )
                 st.write(
                     f"📍Coût net annuel: **{option2_result['net_annual_cost']:.2f} €**"
-                    )
+                )
                 st.write(
                     f"""📍 Coût total sur {self.years} ans: :red[**{option2_result['total_cost_over_years']:.2f} €**] 
                     [loyers: {option2_result['rent_cost_over_years']}, cout inital: 
                     {option2_result['initial_cost_holding_sci']}]"""
                 )
-                st.caption(f"""Notez que le coût de l'IS pourra ici être légèrement inférieur à celui de l'option 1, 
+                st.caption("""Notez que le coût de l'IS pourra ici être légèrement inférieur à celui de l'option 1, 
                         car on déduit les salaires versés aux associés. Le montant de l'IS sera le même dans 
                         les deux cas si le salaire est nul ou identique dans les deux options. Sachant
                         que par contre, le taux de charges sur les salaires est plus avantageux en EURL.""")
 
         with tab2:
-
-            # Visualisations avec Plotly
             st.write("## Comparaison des coûts totaux")
             fig1 = plot_total_costs(
                 option1_result["flat_tax_paid"], option2_result["total_cost_over_years"]
